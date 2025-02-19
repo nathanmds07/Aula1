9b687afc0a6fe4b25177b6a775ccb21f8b788af2
nathanmds07 <135167029+nathanmds07@users.noreply.github.com>
2025-02-19 16:50:34 -0300

    Update Exercicio3.js

--- Exercicio3.js
+++ Exercicio3.js
@@ -1,18 +1,13 @@
-const readline = require("readline"); // importa o módulo readline
+const readline = require("readline");
 
-// cria uma interface de leitura para entrada e saída de dados
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// pedindo para que o usuário informe seu nome e captura o dado
-rl.question("Escreva seu nome: ", (nome) => {
-
-  rl.question("Informe seu salário: ", (salario) => {
-    
+rl.question("Digite o seu nome: ", (nome) => {
+  rl.question("Digite o seu salário R$: ", (salario) => {
     console.log(`O funcionário ${nome} recebe R$${salario} por mês.`);
-    
-    rl.close(); // fecha a interface após a captura dos dados
+    rl.close();
   });
 });
